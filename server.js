e88fe910fa973a52e518283e2609f295ade473b2
Shivam <shivam.gup82@gmail.com>
2022-02-25 11:59:33 +0530

    modified server.js

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.use(express.json());
 
 app.use("/api/todos", require("./routes/api/todos"));
 
-if ((process.env.NODE_ENV = "production")) {
-  app.use(express.static("client/build"));
+// if ((process.env.NODE_ENV = "production")) {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirnanm, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirnanm, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 
